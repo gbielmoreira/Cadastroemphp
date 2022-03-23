7e47f3d1b165b7460204c5ca8b0353a82b920ff5
Gabriel  Moreira <moreiragabriel1707@gmail.com>
2022-03-22 21:49:52 -0300

    Update crud.php

--- Cadastro/crud.php
+++ Cadastro/crud.php
@@ -5,6 +5,7 @@ function getConexao(){
     $dsn="mysql:host=localhost;dbname=bd_app;charset=utf8";
     $user="root";
     $pass="";
+    
     try{
         $pdo = new PDO($dsn, $user, $pass);
         return $pdo;
@@ -17,6 +18,7 @@ function getConexao(){
 }
 
 function insertDB($nome_aplicativo, $tamanho, $data_instalacao, $versao){
+    
     $conn = getConexao();
     $sql = "INSERT INTO tb_aplicativo(nome_aplicativo, tamanho, data_instalacao, versao)
     VALUES (:nome_aplicativo,:tamanho,:data_instalacao,:versao)";
@@ -26,6 +28,7 @@ function insertDB($nome_aplicativo, $tamanho, $data_instalacao, $versao){
     $stmt->bindParam(":tamanho",$tamanho);
     $stmt->bindParam(":data_instalacao",$data_instalacao);
     $stmt->bindParam(":versao",$versao);
+    
     if ($stmt->execute()) {
         echo "<center><strong>SALVO COM SUCESSO</strong></center>";
     }
@@ -34,6 +37,5 @@ function insertDB($nome_aplicativo, $tamanho, $data_instalacao, $versao){
     }
 }
     insertDB($_POST['nome_aplicativo'], $_POST['tamanho'], $_POST['data_instalacao'], $_POST['versao'],);
-
 
 ?>
